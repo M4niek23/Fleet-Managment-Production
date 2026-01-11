4d4e6b9ddb9b1840987fc658a49f58a314191c50
Maniek32 <pat71@o2.pl>
2026-01-11 13:15:47 +0100

    Uporządkowanie projektu

--- Fleet-Managment-Production/Models/Vehicle.cs
+++ Fleet-Managment-Production/Models/Vehicle.cs
@@ -4,7 +4,42 @@ using Microsoft.EntityFrameworkCore;
 
 
 namespace Fleet_Managment_Production.Models
-{
+{   
+    public enum FuelType
+    {
+        [Display(Name = "Benzyna (PB)")]
+        Benzyna,
+
+        [Display(Name = "Olej napędowy (ON)")]
+        Diesel,
+
+        [Display(Name = "Napęd hybrydowy (HEV/PHEV)")]
+        Hybryda,
+
+        [Display(Name = "Napęd elektryczny (BEV)")]
+        Elektryk,
+
+        [Display(Name = "Gaz (LPG/CNG)")]
+        LPG
+    }
+    public enum VehicleStatus
+    {
+        [Display(Name = "Dostępny")]
+        Available,
+
+        [Display(Name = "W użyciu")]
+        InUse,
+
+        [Display(Name = "W serwisie")]
+        InMaintenance,
+
+        [Display(Name = "Sprzedany")]
+        Sold,
+
+        [Display(Name = "Wycofany")]
+        Decommissioned
+    }
+
     [Index(nameof(VIN), IsUnique = true)]
     [Index(nameof(LicensePlate), IsUnique = true)]
     public class Vehicle
