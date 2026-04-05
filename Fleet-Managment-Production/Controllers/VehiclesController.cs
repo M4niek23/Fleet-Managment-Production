837f30482cc82866aac28e6356ad305da07f6b07
Maniek32 <pat71@o2.pl>
2026-04-05 15:54:34 +0200

    Poprawka w wyszukiwaniu w vehicles

--- Fleet-Managment-Production/Controllers/VehiclesController.cs
+++ Fleet-Managment-Production/Controllers/VehiclesController.cs
@@ -58,7 +58,11 @@ namespace Fleet_Managment_Production.Controllers
                     (v.Make != null && v.Make.ToLower().Contains(searchLower)) ||
                     (v.Model != null && v.Model.ToLower().Contains(searchLower)) ||
                     (v.LicensePlate != null && v.LicensePlate.ToLower().Contains(searchLower)) ||
-                    (v.VIN != null && v.VIN.ToLower().Contains(searchLower))
+                    (v.VIN != null && v.VIN.ToLower().Contains(searchLower)) ||
+                    (v.Driver != null && v.Driver.FirstName.ToLower().Contains(searchLower)) ||
+                    (v.Driver != null && v.Driver.LastName.ToLower().Contains(searchLower)) ||
+                    (v.Driver != null && (v.Driver.FirstName + " " + v.Driver.LastName).ToLower().Contains(searchLower)) ||
+                    (v.Driver != null && (v.Driver.LastName + " " + v.Driver.FirstName).ToLower().Contains(searchLower))
                 );
             }
 
