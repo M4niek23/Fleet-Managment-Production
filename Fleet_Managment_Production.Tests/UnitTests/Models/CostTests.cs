ebb110431f9316954da3851f31adc9d21a0f751b
Maniek32 <pat71@o2.pl>
2026-04-19 14:14:51 +0200

    Dodano wstepnie testy do innych modelow, ale nie ma jeszcze tam kodu

---
+++ Fleet_Managment_Production.Tests/UnitTests/Models/CostTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fleet_Managment_Production.Tests.UnitTests.Models
+{
+    internal class CostTests
+    {
+    }
+}
